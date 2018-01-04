36e43066a5b28ba5b83a1d55d96d9c17e6cfc9bc
Jon Olson <jonolson64@outlook.com>
2018-01-03 20:48:13 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+import pygame as pg
+import random
+from settings import *
+
+class Game(object):
+    def __init__(self):
+        # initialize game window, etc.
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.running = True
+
+    def new(self):
+        # start a new game
+        self.all_sprites = pg.sprite.Group()
+        self.run()
+
+    def run(self):
+        # game loop
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        # game loop - update
+        self.all_sprites.update()
+
+    def events(self):
+        # game loop - events
+        for event in pg.event.get():
+            # check for closing window
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+
+    def draw(self):
+        # game loop - draw
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        # *after* drawing everything, flip the display
+        pg.display.flip()
+
+    def show_start_screen(self):
+        # game splash/start screen
+        pass
+
+    def show_go_screen(self):
+        # game over screen
+        pass
+
+if __name__ == '__main__':
+    g = Game()
+    g.show_start_screen()
+    while g.running:
+        g.new()
+        g.show_go_screen()
+
+    pg.quit()
